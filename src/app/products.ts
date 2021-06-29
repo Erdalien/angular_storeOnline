429e0e6f9dd7b6b1cbc28ba7a6cba4b4b5dc8c0d
Erdalien <37020871+Erdalien@users.noreply.github.com>
2021-06-29 17:43:56 +0200

    god please work

---
+++ src/app/products.ts
@@ -0,0 +1,29 @@
+export interface Product {
+  id: number;
+  name: string;
+  price: number;
+  description: string;
+}
+
+export const products = [
+  {
+    id: 1,
+    name: 'Map of Marbleisland Forest',
+    price: 100,
+    description: 'This is a map of mysterious Marble Island'
+  },
+  {
+    id: 2,
+    name: 'Batman',
+    price: 699,
+    description: 'They are stealing my stuff!'
+  },
+  {
+    id: 3,
+    name: 'Hellhound',
+    price: 666,
+    description: 'She is wild antihero'
+  }
+];
+
+
